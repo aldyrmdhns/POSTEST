9208f6ff04d0e05759ceaa7e121749789742f51e
aldyrmdhns <92864319+aldyrmdhns@users.noreply.github.com>
2021-10-20 23:59:44 +0800

    Add files via upload

---
+++ Postest2 - 1.py
@@ -0,0 +1,53 @@
+#Bagian Menu
+def menu():
+    print('---------')
+    print('Main Menu:')
+    print('---------')
+    print('1. USD')
+    print('2. SGD')
+    print('3. EUR')
+    print('4. JPY')
+
+def USD():
+    HargaUSD = 14131
+    rupiah = float(input("Masukkan Uang Rupiah anda = "))
+    Hasil = rupiah / HargaUSD
+    USD = round(Hasil, 2)
+    print("Rp.",rupiah, "==>", USD, 'USD')
+
+def SGD():
+    HargaSGD = 10507
+    rupiah = float(input("Masukkan Uang Rupiah anda = "))
+    Hasil = rupiah / HargaSGD
+    SGD = round(Hasil, 2)
+    print("Rp.",rupiah, "==>", SGD, 'SGD')
+
+def EUR():
+    HargaEUR = 16428
+    rupiah = float(input("Masukkan Uang Rupiah anda = "))
+    Hasil = rupiah / HargaEUR
+    EUR = round(Hasil, 2)
+    print("Rp.",rupiah, "==>", EUR, 'EUR')
+
+def JPY():
+    HargaJPY = 123
+    rupiah = float(input("Masukkan Uang Rupiah anda = "))
+    Hasil = rupiah / HargaJPY
+    JPY = round(Hasil, 2)
+    print("Rp.",rupiah, "==>", JPY, 'JPY')
+
+#Bagian Program Utama
+print('Program Konversi Mata Uang Rupiah')
+menu()
+choice = input('masukan pilihan anda :')
+
+if choice == '1':
+    USD()
+elif choice == '2':
+    SGD()
+elif choice == '3':
+    EUR()
+elif choice == '4':
+    JPY()
+else:
+    print('error')
